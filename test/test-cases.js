83a28b28705f3d7b56a24eed7fd95ac8ef50c864
manab4rd <manabjyoti.sarma.rd@gmail.com>
2019-09-18 18:48:09 +0530

    Adding test casses and demo

---
+++ test/test-cases.js
@@ -0,0 +1,21 @@
+var assert = chai.assert;
+		var expect = chai.expect;
+
+		describe('check jQuery', function() {
+			it('jQuery should defined', function() {
+				assert.isDefined($, "jQuery is defined");
+			});
+		});
+		
+		describe("jQuery.fn.column_search_replace", function(){
+		  it("Check If the table exist", function(){
+			expect($('table')).to.exist;
+		  });
+		  $('table').column_search_replace();
+		  it("Check If two inputs fields are there", function(){
+			expect($('table').parent().find('input')).to.have.length(2)
+		  });
+		  it("Check If one select fields is there", function(){
+			expect($('table').parent().find('select')).to.have.length(1)
+		  });
+		});
